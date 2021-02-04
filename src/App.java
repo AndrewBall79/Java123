fca572a61fff80603fa9922136eaf420fc940776
AndrewBall79 <andy.bob.ball@gmail.com>
2021-02-04 12:44:53 -0600

    robot

--- src/App.java
+++ src/App.java
@@ -1,8 +1,25 @@
+class Robot{
+    public void speak(String text) {
+        System.out.println(text);
+    }
+    public void jump(int height){
+        System.out.println("Jumping: " + height + " feet");
+    }
+    public void move(String direction, double distance) {
+        System.out.println("Moving " + distance + " meters " + direction );
 
+    }
+}
 
 public class App {
 
     public static void main(String[] args) {
+        Robot sam = new Robot();
+        sam.speak("Hi I'm SAM.");
+        sam.jump(7);
+        sam.move("west", 1.5);
+        String greeting = "Hello There";
+        sam.speak(greeting);
 
     }
 }
--- src/tut03.java
+++ src/tut03.java
@@ -1,31 +1,41 @@
 import java.util.Scanner;
-
 class Person {
     //Instance variables (data or "state")
     String name;
-    int age;
-
+    int birthYear;
     //classes can contain
     //1. Data
     //2 Subroutine (methods)
     void speak() {
-        
+        System.out.println("My name is " + name + " and I am " + getAge() + " years old");
+    }
+
+    void sayHello(){
+        System.out.println("Hello there!");
+    }
+
+    int getAge(){
+        return 2021 - birthYear;
+    }
+
+    int calculateYearsToRetirement() {
+        return 65 - getAge();
+    }
+    String getName(){
+        return name;
     }
 }
 
 public class tut03 {
-
     public static void main(String[] args) {
-
 
         Person person1 = new Person();
         person1.name = "Joe Bloggs";
-        person1.age = 37;
+        person1.birthYear = 1979;
+        person1.sayHello();
+        person1.speak();
 
-        Person person2 = new Person();
-        person2.name = "Sarah Smith";
-        person2.age = 35;
-
-        System.out.println(person1.name);
+        int years = person1.calculateYearsToRetirement();
+        System.out.println("I have " + years + " years until retirement.");
     }
 }
