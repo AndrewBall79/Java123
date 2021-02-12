34099549f83f3250b248760fbe6e5f8897e0da8f
AndrewBall79 <andy.bob.ball@gmail.com>
2021-02-12 13:10:01 -0600

    Generics

--- Encapsulation/src/App.java
+++ Encapsulation/src/App.java
@@ -1,5 +1,11 @@
-class Plant{
+import java.util.ArrayList;
+import java.util.HashMap;
 
+class Animal{
+
+}
+
+class Plant{
     public static final int ID = 7;
     private String name;
 
@@ -22,7 +28,7 @@ class Plant{
 
 class Machine{
     public void start(){
-        System.out.println("Machine Started");
+        System.out.println("Machine farted");
     }
 }
 
@@ -37,6 +43,32 @@ class Camera extends Machine{
 
 public class App {
     public static void main(String[] args) {
+        // Old way of doing things.
+        ArrayList list = new ArrayList();
+        list.add("apple");
+        list.add("banana");
+        list.add("orange");
+
+        String fruit = (String) list.get(1);
+
+        // New way of doing things.
+        ArrayList<String> strings = new ArrayList<String>();
+
+        strings.add("cat");
+        strings.add("dog");
+        strings.add("alligator");
+
+        String animal = strings.get(1);
+
+        // There can be more than one type argument
+
+        HashMap<Integer, String > map = new HashMap<Integer, String>();
+
+        /// Java 7 now infers the second parameterized class///
+
+        ArrayList<Animal> someList = new ArrayList<>();
+
+
         byte byteValue = 127;
         short shortValue = 32767;
         int intValue = 2147483647;
@@ -47,19 +79,19 @@ public class App {
     Machine machine1 = new Machine();
     Camera camera1 = new Camera();
 
-    camera1.start();
-    machine1.start();
-    camera1.snap();
+//    camera1.start();
+//    machine1.start();
+//    camera1.snap();
 
     //Upcasting
         Machine machine2 = camera1;
-        machine2.start();
+//        machine2.start();
 
 
         //Downcasting is inherently unsafe
         Machine machine3 = new Camera();
         Camera camera2 = (Camera) machine3;
-        camera2.snap();
+//        camera2.snap();
 
         Machine machine4 = new Machine();
 
