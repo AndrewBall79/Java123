b9629325240392247050e91b95d28b6053f0f046
AndrewBall79 <andy.bob.ball@gmail.com>
2021-02-11 18:11:00 -0600

    notes

--- src2/src/Info.java
+++ src2/src/Info.java
@@ -1,3 +1,9 @@
+//private === only within same class
+//public --- from anywhere
+//protected --- same class, subclass, and same package
+//no modifier --- same package only
+
+
 public interface Info {
     public void showInfo();
 }
