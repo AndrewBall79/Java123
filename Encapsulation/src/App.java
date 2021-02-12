815972284b5ff19d75c24bfdb11810bad6a658c3
AndrewBall79 <andy.bob.ball@gmail.com>
2021-02-12 12:42:55 -0600

    upcasting downcasting

--- Encapsulation/src/App.java
+++ Encapsulation/src/App.java
@@ -20,6 +20,20 @@ class Plant{
     }
 }
 
+class Machine{
+    public void start(){
+        System.out.println("Machine Started");
+    }
+}
+
+class Camera extends Machine{
+    public void start(){
+        System.out.println("Camera Started");
+    }
+    public void snap(){
+        System.out.println("Photo taken");
+    }
+}
 
 public class App {
     public static void main(String[] args) {
@@ -30,6 +44,24 @@ public class App {
         float floatValue = 789.59f;
         double doubleValue = 10^208;
 
-        System.out.println(Math.round(floatValue));
+    Machine machine1 = new Machine();
+    Camera camera1 = new Camera();
+
+    camera1.start();
+    machine1.start();
+    camera1.snap();
+
+    //Upcasting
+        Machine machine2 = camera1;
+        machine2.start();
+
+
+        //Downcasting is inherently unsafe
+        Machine machine3 = new Camera();
+        Camera camera2 = (Camera) machine3;
+        camera2.snap();
+
+        Machine machine4 = new Machine();
+
     }
 }
