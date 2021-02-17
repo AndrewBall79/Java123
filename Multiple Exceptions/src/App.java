7cb69ce0ce70daf9abd5de1341afaca2db3fa10c
AndrewBall79 <andy.bob.ball@gmail.com>
2021-02-17 15:24:05 -0600

    Multiple Exceptions

---
+++ Multiple Exceptions/src/App.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.text.ParseException;
+
+public class App {
+    public static void main(String[] args) {
+        Test test = new Test();
+
+//        try {
+//            test.run();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (ParseException e) {
+//            System.out.println("Couldn't Parse File");
+//        }
+
+//        try {
+//            test.run();
+//        } catch (IOException | ParseException e) {
+//            e.printStackTrace();
+//        }
+
+        try {
+            test.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            test.input();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
