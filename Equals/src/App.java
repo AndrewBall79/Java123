9c5c3b95fe0464e3b9877f6dbd6e4d9f0c86eb3a
AndrewBall79 <andy.bob.ball@gmail.com>
2021-02-18 17:29:35 -0600

    inner classes

--- Equals/src/App.java
+++ Equals/src/App.java
@@ -31,29 +31,37 @@ class Person{
 
 public class App {
     public static void main(String[] args) {
+        Robot robot1 = new Robot(7);
+        robot1.start();
+
+        Robot.Battery battery = new Robot.Battery();
+        battery.charge();
+
 
         //shows Hashcode
-        System.out.println(new Object());
+//        System.out.println(new Object());
 
         Person person1 = new Person(5, "Bob");
         Person person2 = new Person(5, "Bob");
 
         //true
-        System.out.println(person1.equals(person2));
+//        System.out.println(person1.equals(person2));
 
         Double value1 = 7.2;
         Double value2 = 7.2;
 
-        //false
-        System.out.println(value1 == value2);
+//        false
+//        System.out.println(value1 == value2);
 
         //true
-        System.out.println(value1.equals(value2));
+//        System.out.println(value1.equals(value2));
 
         String text1 = "Hello";
         String text2 = "Hello";
 
         //true
-        System.out.println(text1 == text2);
+//        System.out.println(text1 == text2);
     }
+
+
 }
