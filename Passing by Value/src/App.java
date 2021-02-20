f53d7f448a54597f3fb96e246212cb6358f1f6fa
AndrewBall79 <andy.bob.ball@gmail.com>
2021-02-19 20:43:00 -0600

    lists

--- Passing by Value/src/App.java
+++ Passing by Value/src/App.java
@@ -1,8 +1,39 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<Integer>();
+        List<Integer> linkedList = new LinkedList<Integer>();
+
+        doTimings("ArrayList", numbers);
+        doTimings("LinkedList", linkedList);
+
+
+        numbers.add(10);
+        numbers.add(100);
+        numbers.add(40);
+
+        System.out.println(numbers.get(0));
+
+        //Indexed for loop
+        for(int i=0; i<numbers.size(); i++){
+        }
+
+        //enhanced for loop
+        for(Integer value: numbers){
+        }
+
+        //Removing items (careful)
+        numbers.remove(numbers.size() -1);
+
         App app = new App();
 
+        // List Interface
+        List<String> values = new ArrayList<String>();
 
+        //Linked Lists
 
         int value = 7;
         System.out.println("1. Value is: " + value);
@@ -38,5 +69,26 @@ public class App {
         person = new Person("mic");
 
         System.out.println("3. Person is" + person);;
+    }
+
+    private static void doTimings(String type, List<Integer> list){
+        for(int i=0; i<15; i++){
+            list.add(i);
+        }
+        long start = System.currentTimeMillis();
+
+//        add items at end of list
+        for(int i = 0; i<15; i++){
+            list.add(i);
+        }
+
+//        add items at beginning of list
+        for(int i = 0; i<15; i++){
+            list.add(0, i);
+        }
+
+        long end = System.currentTimeMillis();
+
+        System.out.println("Time Taken: "+ (end - start) + " ms for " + type);
     }
 }
