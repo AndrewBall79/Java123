0544fce2e45012ba2210ef566cfa9e7836536220
AndrewBall79 <andy.bob.ball@gmail.com>
2021-02-22 19:57:55 -0600

    more sets

--- Sets/src/App.java
+++ Sets/src/App.java
@@ -3,8 +3,13 @@ import java.util.*;
 public class App {
     public static void main(String[] args) {
 
+
         //Hast set does not retain order.
         Set<String> set1 = new HashSet<String>();
+
+        if (set1.isEmpty()){
+            System.out.println("Set is empty");
+        }
 
         set1.add("dog");
         set1.add("cat");
@@ -36,8 +41,55 @@ public class App {
 
         System.out.println(set3);
 
+        // Iteration
+
         for(Integer element: set3){
             System.out.println(element);
         }
+
+        // Does set contain a given item?
+
+        if (set1.contains("aardvark")){
+            System.out.println("Contains aardvark");
+        }else {
+            System.out.println("Does not contain aardvark");
+        }
+        if (set1.contains("cat")){
+            System.out.println("Contains cat");
+        } else {
+            System.out.println("Does not contain cat");
+        }
+        if (set1.isEmpty()){
+            System.out.println("Set is empty");
+        }else{
+            System.out.println("Set contains items: " + set1);
+        }
+
+        // Intersection //
+
+        Set<String> set4 = new HashSet<String>();
+
+        set4.add("dog");
+        set4.add("cat");
+        set4.add("giraffe");
+        set4.add("monkey");
+        set4.add("ant");
+
+        Set<String> intersection = new HashSet<String>(set1);
+
+        intersection.retainAll(set4);
+
+        System.out.println("Intersection of set 1 and set 4: " + intersection);
+
+
+        /// Difference ///
+
+        Set<String> difference = new HashSet<String>(set1);
+
+        difference.removeAll(set4);
+
+        System.out.println();
+
+        System.out.println(difference);
     }
 }
