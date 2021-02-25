9830fac9d05dc4ada7b999cbea6d940487a6d9ee
AndrewBall79 <andy.bob.ball@gmail.com>
2021-02-25 13:05:49 -0600

    order by length with catch all

--- Natural Ordering/src/App.java
+++ Natural Ordering/src/App.java
@@ -27,7 +27,16 @@ class Person implements Comparable<Person>{
 
     @Override
     public int compareTo(Person person) {
-        return -name.compareTo(person.name);
+        int len1 = name.length();
+        int len2 = person.name.length();
+        if (len1 > len2) {
+            return 1;
+        } else if (len1 < len2) {
+            return -1;
+        } else {
+            //use alphabetical order if names are equal length
+            return name.compareTo(person.name);
+        }
     }
 }
 
