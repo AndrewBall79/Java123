3bac227dec8a67a84c2af0623e3a3c67a3acd437
AndrewBall79 <andy.bob.ball@gmail.com>
2021-02-25 12:58:11 -0600

    reverse alphabetical

--- Natural Ordering/src/App.java
+++ Natural Ordering/src/App.java
@@ -1,11 +1,40 @@
 import java.util.*;
 
+class Person implements Comparable<Person>{
+    private String name;
 
+    public Person(String name){
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public int compareTo(Person person) {
+        return -name.compareTo(person.name);
+    }
+}
 
 public class App {
     public static void main(String[] args) {
-        List<String> list = new ArrayList<String>();
-        Set<String> set = new TreeSet<String>();
+        List<Person> list = new ArrayList<Person>();
+        Set<Person> set = new TreeSet<Person>();
 
         addElements(list);
         addElements(set);
@@ -16,16 +45,16 @@ public class App {
         System.out.println();
         System.out.println(set);
     }
-    private static void addElements(Collection<String> col){
-        col.add("Joe");
-        col.add("Sue");
-        col.add("Ponce");
-        col.add("Celine");
-        col.add("Petunia");
+    private static void addElements(Collection<Person> col){
+        col.add(new Person("Joe"));
+        col.add(new Person("Sue"));
+        col.add(new Person("Ponce"));
+        col.add(new Person("Celine"));
+        col.add(new Person("Petunia"));
     }
 
-    private static void showElements(Collection<String> col){
-        for(String element: col){
+    private static void showElements(Collection<Person> col){
+        for(Person element: col){
             System.out.println(element);
         }
     }
