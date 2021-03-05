9323d997304de344bc72c2cb5918e561dab89fd4
AndrewBall79 <andy.bob.ball@gmail.com>
2021-03-05 11:28:47 -0600

    Iterables

--- Queues/src/App.java
+++ Queues/src/App.java
@@ -3,11 +3,26 @@ import java.util.concurrent.ArrayBlockingQueue;
 
 public class App {
     public static void main(String[] args) {
-        Queue<Integer> q1 = new ArrayBlockingQueue<Integer>(3);
+        Queue<Integer> q1 = new ArrayBlockingQueue<Integer>(30);
 
-        q1.add(10);
+        q1.add(1);
+        q1.add(2);
+        q1.add(3);
+        q1.add(4);
+        q1.add(5);
         q1.add(6);
+        q1.add(7);
         q1.add(8);
+        q1.add(9);
+        q1.add(10);
+        q1.add(11);
+        q1.add(12);
+
+        Iterator it = q1.iterator();
+
+
+
+        System.out.println(it);
 
         try {
             q1.add(90);
@@ -19,8 +34,6 @@ public class App {
         }
 
         System.out.println("Removed from queue: " + q1.remove());
-        System.out.println("Removed from queue: " + q1.remove());
-        System.out.println("Removed from queue: " + q1.remove());
 
         try {
         System.out.println("Removed from queue: " + q1.remove());
@@ -29,22 +42,23 @@ public class App {
             System.out.println("Tried to remove too many items");
         }
 
+        System.out.println(it);
 
         Queue<Integer> q2 = new ArrayBlockingQueue<Integer>(2);
 
-        System.out.println("Queue 2 peek : " + q2.peek());
-
-
-
-        q2.offer(10);
-        q2.offer(6);
-        if( q2.offer(8) == false){
-            System.out.println("Offer failed to add third item");
-        }
-        for (Integer value: q2){
-            System.out.println("Queue 2 value: " + value);
-        }
-
-        System.out.println("Queue 2 poss: " + q2.poll() );
+//        System.out.println("Queue 2 peek : " + q2.peek());
+//
+//
+//
+//        q2.offer(10);
+//        q2.offer(6);
+//        if( q2.offer(8) == false){
+//            System.out.println("Offer failed to add third item");
+//        }
+//        for (Integer value: q2){
+//            System.out.println("Queue 2 value: " + value);
+//        }
+//
+//        System.out.println("Queue 2 poss: " + q2.poll() );
     }
 }
